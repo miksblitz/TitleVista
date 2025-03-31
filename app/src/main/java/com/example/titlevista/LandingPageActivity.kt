e52f306e943f7775d4412a8251ff14f60417e11c
leyuumm_ <monleonkhenz@gmail.com>
2025-03-31 22:27:27 +0800

    Some changes to the Main Launcher, and its compatibility

--- app/src/main/java/com/example/titlevista/LandingPageActivity.kt
+++ app/src/main/java/com/example/titlevista/LandingPageActivity.kt
@@ -1,17 +1,15 @@
 package com.example.titlevista
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LandingPageActivity : Activity() {
+class LandingPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContentView(R.layout.activity_landing_page)
-
     }
 }
